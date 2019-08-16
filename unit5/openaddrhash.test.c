35539fc744c24c1b20e6fed358600661ebadc76b
zuquanxie <designer_xzq@163.com>
2019-08-16 09:47:07 +0800

    修改openaddrhash.test

--- unit5/openaddrhash.test.c
+++ unit5/openaddrhash.test.c
@@ -4,35 +4,65 @@
 #include "openaddrhash.h"
 #define Wait(x) WaitClocks(x*CLOCKS_PER_SEC)
 void WaitClocks(clock_t t);
+void CleanLines(unsigned int lineCount);
 void ListTable(HashTable H);
 
 int main()
 {
 	HashTable H;
+	unsigned int i, lineCount;
+	int numbers[] = {6, 23, 17, 32, 8, 7, 19, 13, 35, 27, 28, 31, 5, 12, 37, 1, 14};
 
-	H = CreateHashTable(13);
-	HashTableInsert(H, 23);
-	ListTable(H);
-	Wait(1);
-	HashTableInsert(H, 21);
-	ListTable(H);
-	Wait(1);
-	HashTableInsert(H, 83);
-	ListTable(H);
+	H = CreateHashTable(39);
+	i = 0;
+	lineCount = 12;
+	while (i < 17)
+	{
+		HashTableInsert(H, numbers[i]);
+		CleanLines(lineCount);
+		printf("Insert: %02i\n", numbers[i]);
+		ListTable(H);
+		i++;
+		Wait(2);
+	}
 
 	return 0;
 }
 
 void ListTable(HashTable H)
 {
-	int i;
+	unsigned int i, j;
 
 	i = 0;
 	while (i < H->TableSize)
 	{
-		printf("%i: %i\n", i, H->Cells[i].Element);
+		j = 0;
+		while (j < 4 && i < H->TableSize)
+		{
+			if (H->Cells[i].Info == Empty)
+				printf("%02i: %s", i, "--");
+			else
+				printf("%02i: %02i", i, H->Cells[i].Element);
+			printf("    ");
+			j++;
+			i++;
+		}
+		printf("\n");
+	}
+}
+
+void CleanLines(unsigned int lineCount)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < lineCount)
+	{
+		putchar('\r');
+		putchar('\b');
 		i++;
 	}
+
 }
 
 void WaitClocks(clock_t t)
